20e8070fe7908994bb6056de30d9535413c063cf
nullenemy <null.enemy@gmail.com>
2019-08-08 09:15:53 +0800

    start 07-basic-web-app

---
+++ 07-basic-web-app/src/server.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    var filePath = './public' + request.url;
+
+    if (filePath == './public/') {
+        filePath = './public/index.html';
+    }
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml',
+        '.wasm': 'application/wasm'
+    }
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    console.log(filePath);
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+
+            if(error.code == 'ENOENT') {
+                fs.readFile('./public/404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
